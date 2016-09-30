4a9780ae294139cbf4b0b53880aff33b15bb0e5f
jciancio <jciancio7@gmail.com>
2016-09-30 16:16:44 -0400

    updated root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # concern :active_scaffold_association, ActiveScaffold::Routing::Association.new
-  # concern :active_scaffold, ActiveScaffold::Routing::Basic.new(association: true)
+  root 'users#index'
+
   resources :users do as_routes end
 
   namespace :api do
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
       resources :users
     end
   end
+  # concern :active_scaffold_association, ActiveScaffold::Routing::Association.new
+  # concern :active_scaffold, ActiveScaffold::Routing::Basic.new(association: true)
 end
